6dd5125fadb6b447a5f3789973fbfeb7c8fabccf
Polo064 <ognjengames06@gmail.com>
2023-06-21 12:04:51 +0200

    a
    a

--- Prodavnica_Filmova2/Prodavnica_Filmova2/AdminLogin.cs
+++ Prodavnica_Filmova2/Prodavnica_Filmova2/AdminLogin.cs
@@ -11,39 +11,49 @@ using System.Windows.Forms;
 using IronXL;
 using Prodavnica_Filmova2.Repository;
 using Prodavnica_Filmova2.Tools;
+using Prodavnica_Filmova2.ValidationClasses;
 
 namespace Prodavnica_Filmova2
 {
     public partial class AdminLogin : Form
     {
         private AdminRepository _adminRepository;
+        private AdminLoginValidation _adminLoginValidation;
         public AdminLogin()
         {
             InitializeComponent();
             _adminRepository = new AdminRepository(ConfigUtil.GetWorksheetPath());
+            _adminLoginValidation = new AdminLoginValidation();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (_adminRepository.IsAdministrator(textBox_username.Text, textBox_password.Text))
-            {               
-                string message = "Uspesno ste se ulogovali!";
-                string title = "Uspeh";
-                MessageBoxButtons button = MessageBoxButtons.OK;
-                DialogResult result = MessageBox.Show(message, title, button);
-                if (result == DialogResult.OK)
-                {
-                    var adminWindow = new AdminWindow();
-                    this.Hide();
-                    adminWindow.Show();
-                }
-                else { }
+            if (_adminLoginValidation.IsEmpty(textBox_username.Text, textBox_password.Text))
+            {
+                label3.Text = "Niste uneli sve podatke";
             }
             else
             {
-                label3.Text = "Niste uneli dobar username ili password";
-                textBox_username.Text = string.Empty;
-                textBox_password.Text = string.Empty;
+                if (_adminRepository.IsAdministrator(textBox_username.Text, textBox_password.Text))
+                {
+                    string message = "Uspesno ste se ulogovali!";
+                    string title = "Uspeh";
+                    MessageBoxButtons button = MessageBoxButtons.OK;
+                    DialogResult result = MessageBox.Show(message, title, button);
+                    if (result == DialogResult.OK)
+                    {
+                        var adminWindow = new AdminWindow();
+                        this.Hide();
+                        adminWindow.Show();
+                    }
+                    else { }
+                }
+                else
+                {
+                    label3.Text = "Niste uneli dobar username ili password";
+                    textBox_username.Text = string.Empty;
+                    textBox_password.Text = string.Empty;
+                }
             }
         }
     }
---
+++ Prodavnica_Filmova2/Prodavnica_Filmova2/ValidationClasses/AdminLoginValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodavnica_Filmova2.ValidationClasses
+{
+    class AdminLoginValidation
+    {
+        public AdminLoginValidation()
+        {
+
+        }
+        public bool IsEmpty(string username, string password)
+        {
+            if (username == "" || password == "")
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
